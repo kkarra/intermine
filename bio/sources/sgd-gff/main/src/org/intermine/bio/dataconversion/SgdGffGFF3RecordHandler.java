780cba1679bdf47ac22bc8587ccb1780f0f14007
Kalpana Karra <kkarra@stanford.edu>
2014-01-29 12:53:46 -0800

    save-test

--- bio/sources/sgd-gff/main/src/org/intermine/bio/dataconversion/SgdGffGFF3RecordHandler.java
+++ bio/sources/sgd-gff/main/src/org/intermine/bio/dataconversion/SgdGffGFF3RecordHandler.java
@@ -20,6 +20,7 @@ import org.intermine.bio.dataconversion.IdResolver;
 import org.intermine.bio.io.gff3.GFF3Record;
 import org.intermine.metadata.Model;
 import org.intermine.xml.full.Item;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -41,6 +42,7 @@ public class SgdGffGFF3RecordHandler extends GFF3RecordHandler
      */
     public SgdGffGFF3RecordHandler (Model model) {
         super(model);
+        refsAndCollections.put("mRNA", "gene");
     }
 
     /**
@@ -48,47 +50,72 @@ public class SgdGffGFF3RecordHandler extends GFF3RecordHandler
      */
     @Override
     public void process(GFF3Record record) {
-        // This method is called for every line of GFF3 file(s) being read.  Features and their
-        // locations are already created but not stored so you can make changes here.  Attributes
-        // are from the last column of the file are available in a map with the attribute name as
-        // the key.   For example:
-        //
-        //     Item feature = getFeature();
-        //     String symbol = record.getAttributes().get("symbol");
-        //     feature.setAttrinte("symbol", symbol);
-        //
-        // Any new Items created can be stored by calling addItem().  For example:
-        // 
-        //     String geneIdentifier = record.getAttributes().get("gene");
-        //     gene = createItem("Gene");
-        //     gene.setAttribute("primaryIdentifier", geneIdentifier);
-        //     addItem(gene);
-        //
-        // You should make sure that new Items you create are unique, i.e. by storing in a map by
-        // some identifier. 
-    	  	
-        getFeature().setClassName("TFBindingSite");
-        Item bindingSite = getFeature();
-        String name = record.getId();
-        String factorGeneName = record.getAttributes().get("binding_factor").get(0);
-        
-        //if (!("unknown").equals(factorGeneName.toLowerCase()) && !("unspecified").equals(factorGeneName.toLowerCase())) {
-            Item gene = getGene(factorGeneName);
-            if (gene != null) {
-                bindingSite.setReference("factor", gene.getIdentifier());
-            }
-       // }
+    	// This method is called for every line of GFF3 file(s) being read.  Features and their
+    	// locations are already created but not stored so you can make changes here.  Attributes
+    	// are from the last column of the file are available in a map with the attribute name as
+    	// the key.   For example:
+    	//
+    	//     Item feature = getFeature();
+    	//     String symbol = record.getAttributes().get("symbol");
+    	//     feature.setAttrinte("symbol", symbol);
+    	//
+    	// Any new Items created can be stored by calling addItem().  For example:
+    	// 
+    	//     String geneIdentifier = record.getAttributes().get("gene");
+    	//     gene = createItem("Gene");
+    	//     gene.setAttribute("primaryIdentifier", geneIdentifier);
+    	//     addItem(gene);
+    	//
+    	// You should make sure that new Items you create are unique, i.e. by storing in a map by
+    	// some identifier. 
 
- 
+    	//chrVIII	Pelechano_2013	mRNA	486202	486672	.	-	.	
+    	//ID=Pelechano_chrVIII_486672_486202;Name=YHR191C.95;glu_count=0;gal_count=3;Note=Covering_one_intact_ORF;5pScore=1;3pScore=1;Parent=YHR191C
+
+    	Item transcript = getFeature(); //class name is MRNA
+
+    	String parentGene = record.getAttributes().get("Parent").get(0);
+
+    	String glu_count = record.getAttributes().get("glu_count").get(0);
+    	String gal_count = record.getAttributes().get("gal_count").get(0);
+    	String note = record.getAttributes().get("Note").get(0);
+    	String fivescore = record.getAttributes().get("5pScore").get(0);
+    	String threescore = record.getAttributes().get("3pScore").get(0);
+
+
+    	Item gene = getGene(parentGene);
+    	if (gene != null) { 	
+
+    		if(StringUtils.isNotEmpty(glu_count)){
+    			transcript.setAttribute("gluCount",glu_count);                    
+    		}
+    		if(StringUtils.isNotEmpty(gal_count)){
+    			transcript.setAttribute("galCount",gal_count);                    
+    		}                
+    		if(StringUtils.isNotEmpty(note)){
+    			transcript.setAttribute("note",note);                    
+    		}                
+    		if(StringUtils.isNotEmpty(fivescore)){
+    			transcript.setAttribute("fivePrimeScore",fivescore);                    
+    		}                
+    		if(StringUtils.isNotEmpty(threescore)){
+    			transcript.setAttribute("threePrimeScore",threescore);                    
+    		}
+    		
+    		transcript.setReference("gene", gene.getIdentifier());
+
+    	}
+
+
     }
 
-    private Item getGene(String symbol) {
+    private Item getGene(String secondaryIdentifier) {
  
-        Item gene = geneIdMap.get(symbol);
+        Item gene = geneIdMap.get(secondaryIdentifier);
         if (gene == null) {
             gene = converter.createItem("Gene");
-            geneIdMap.put(symbol, gene);
-            gene.setAttribute("symbol", symbol);
+            geneIdMap.put(secondaryIdentifier, gene);
+            gene.setAttribute("secondaryIdentifier", secondaryIdentifier);
             gene.setReference("organism", getOrganism());
             addItem(gene);
         }
