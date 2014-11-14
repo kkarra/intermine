4e9c70aa2800b48fcdb654cbc91c4400a5326e7c
Kalpana Karra <kkarra@stanford.edu>
2014-11-14 14:19:11 -0800

    save

--- bio/sources/sgd-protein-abundance/main/src/org/intermine/bio/dataconversion/SgdProteinAbundanceConverter.java
+++ bio/sources/sgd-protein-abundance/main/src/org/intermine/bio/dataconversion/SgdProteinAbundanceConverter.java
@@ -115,7 +115,10 @@ public class SgdProteinAbundanceConverter extends BioFileConverter
 			String gfptagged = line[3].trim();
 			String gfpvisualized =  line[4].trim(); 
 			String tapvisualized = line[5].trim();
-			String abundance = line[6].trim() + " molecules/cell";
+			String abundance = line[6].trim();
+			if(!abundance.equalsIgnoreCase("not quantitated") || !abundance.equalsIgnoreCase("not visualized")) {
+				abundance += " molecules/cell";
+			}
 			String error = line[7].trim();
 			String localization = line[8].trim();
 
